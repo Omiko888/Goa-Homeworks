98519b92332d7df7078aa9adfcc468a925d6864d
Omiko888 <omiko096@gmail.com>
2024-07-09 19:28:07 +0400

    first commit

---
+++ Level 03/homework/level3.py
@@ -0,0 +1,43 @@
+name = 'Omiko'
+
+last_name = 'Barkalaia'
+
+age = '12'
+
+hobby = 'Football'
+
+country = 'Georgia'
+
+print(name, last_name, age, hobby, country)
+
+
+name = 'Omiko'
+
+last_name = 'Barkalaia'
+
+print(name + last_name)
+
+#მიმატება
+
+print(15 + 27)     
+print(30 + 415)
+
+#გამოკლება
+
+print(521 - 272)
+print(45 - 12)
+
+#გამრავლება
+
+print(32 * 3)
+print(42 * 5)
+
+#გაყოფა
+
+print(30 // 5)
+print(210 // 2)
+
+
+print(10 ** 3) #10 * 10 * 10
+print(30 ** 4) #30 * 30 * 30 * 30
+
